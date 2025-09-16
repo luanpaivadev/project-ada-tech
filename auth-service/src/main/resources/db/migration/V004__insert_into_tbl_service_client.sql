8c416dd2fb0fd7eccc0989cfb90936ffa9e1bb5d
luanpaivadev <luanpaivadev@gmail.com>
2025-09-16 19:42:50 -0300

    Create V004__insert_into_tbl_service_client.sql

---
+++ auth-service/src/main/resources/db/migration/V004__insert_into_tbl_service_client.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.tbl_service_client
+(id, client_id, client_secret, roles)
+VALUES('4c1da05b-449d-45e6-8b17-46d9e2ecf6ee'::uuid, 'order-service', '$2a$12$hGtgI7StJixlD4CCHvlcbOcaQQ4aEGXk8CcYoiHZNTyakNRsA/Zmq', 'ROLE_SERVICE');
+
+INSERT INTO public.tbl_service_client
+(id, client_id, client_secret, roles)
+VALUES('e164e48d-795e-4242-9093-6a1774eb6503'::uuid, 'invoice-service', '$2a$12$5eUzpCEKWhqG2WFqq.LOA.SBNn/ZxXoP10TK3MMblc1gq1vDV5fXe', 'ROLE_SERVICE');
