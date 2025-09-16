b6b7b9389604edbc8756f18bd124f94b16e9e150
luanpaivadev <luanpaivadev@gmail.com>
2025-09-16 20:55:22 -0300

    Update PaymentGatewayServicePort.java

--- order-service/src/main/java/com/luanpaiva/order_service/core/ports/out/PaymentGatewayServicePort.java
+++ order-service/src/main/java/com/luanpaiva/order_service/core/ports/out/PaymentGatewayServicePort.java
@@ -2,5 +2,5 @@ package com.luanpaiva.order_service.core.ports.out;
 
 public interface PaymentGatewayServicePort {
 
-    Boolean validateClientSecret(String clientSecret);
+    void validateClientSecret(String clientSecret);
 }
