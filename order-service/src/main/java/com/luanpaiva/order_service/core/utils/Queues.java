26c26d841cf497b2eb8f548e67ece817dd4130cb
luanpaivadev <luanpaivadev@gmail.com>
2025-09-16 21:19:48 -0300

    Update Queues.java

--- order-service/src/main/java/com/luanpaiva/order_service/core/utils/Queues.java
+++ order-service/src/main/java/com/luanpaiva/order_service/core/utils/Queues.java
@@ -2,6 +2,7 @@ package com.luanpaiva.order_service.core.utils;
 
 public abstract class Queues {
 
+    public static final String PRODUCT_PAYMENT_APPROVED_QUEUE = "product.payment_approved";
     public static final String NOTIFICATION_STATUS_ORDER_QUEUE = "notification.status_order";
 
     Queues() throws IllegalAccessException {
